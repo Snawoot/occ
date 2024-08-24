960926b87570152e9cd8e92a96e0725757b4959a
Vladislav Yarmak <vladislav@vm-0.com>
2024-08-24 15:41:04 +0300

    initial code

---
+++ cas.go
@@ -0,0 +1,25 @@
+package cas
+
+import "sync/atomic"
+
+type Container[T any] atomic.Pointer[T]
+
+func Wrap[T any](ptr *T) *Container[T] {
+	c := new(atomic.Pointer[T])
+	c.Store(ptr)
+	return (*Container[T])(c)
+}
+
+func (c *Container[T]) Value() *T {
+	return (*atomic.Pointer[T])(c).Load()
+}
+
+func (c *Container[T]) Update(txn func(oldValue *T) *T) {
+	for {
+		oldValue := c.Value()
+		newValue := txn(oldValue)
+		if (*atomic.Pointer[T])(c).CompareAndSwap(oldValue, newValue) {
+			break
+		}
+	}
+}
